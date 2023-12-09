581b204f2132fffa9279f579dfd758088b32dd23
Javeria Rehman <jia.rehman122@gmail.com>
2023-12-09 17:02:14 +0500

    Controller created and Variables created

---
+++ PetPlanetWebApp/Server/Controllers/PetfinderAuthController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetPlanetWebApp.Server.Services;
+using PetPlanetWebApp.Shared.Utils;
+using PetPlanetWebApp.Shared.Utils;
+
+namespace PetPlanetWebApp.Server.Controllers
+{
+    public class PetfinderAuthController : Controller
+    {
+        private readonly PetfinderAuthService _petfinderAuthService;
+
+        public PetfinderAuthController(PetfinderAuthService petfinderAuthService)
+        {
+            _petfinderAuthService = petfinderAuthService;
+        }
+
+        public async Task<IActionResult> YourAction()
+        {
+            try
+            {
+                string accessToken = await _petfinderAuthService.GetAccessTokenAsync();
+
+                // Use the accessToken as needed (e.g., for subsequent API requests)
+                // ...
+
+                Variables.TOKEN = accessToken;
+
+                return Ok($"Access Token: {accessToken}");
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions
+                return StatusCode(500, "Error fetching access token from Petfinder API");
+            }
+        }
+    }
+
+}
---
+++ PetPlanetWebApp/Shared/Utils/Variables.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetPlanetWebApp.Shared.Utils
+{
+    public class Variables
+    {
+        public static string TOKEN;
+    }
+}
