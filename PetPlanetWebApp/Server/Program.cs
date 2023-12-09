f9cb963fbd09a185f9ea4500a8097e68d4a00347
Javeria Rehman <jia.rehman122@gmail.com>
2023-12-09 17:28:47 +0500

    Token Display with error// I AM CRYING

--- PetPlanetWebApp/Server/Program.cs
+++ PetPlanetWebApp/Server/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.ResponseCompression;
+using PetPlanetWebApp.Server.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+builder.Services.AddScoped<PetfinderAuthService>();
 
 var app = builder.Build();
 
