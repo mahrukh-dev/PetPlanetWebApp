2a914d1937b24bcff6b28ba62ac8461ecd744f34
MahRukh <mahrukhdev@gmail.com>
2023-12-13 22:27:40 +0500

    pet class added

---
+++ PetPlanetWebApp/Shared/Models/Pet.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetPlanetWebApp.Shared.Models
+{
+    public class Pet
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Species { get; set; }
+        public string Breed { get; set; }
+        public string Color { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public string Size { get; set; }
+        public string Coat { get; set; }
+        public string Description { get; set; }
+        public string ContactEmail { get; set; }
+        public string ContactPhone { get; set; }
+        public string ContactAddress { get; set; }
+        public byte[] Photo { get; set; }
+    }
+
+}
