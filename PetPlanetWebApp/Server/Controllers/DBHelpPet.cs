f58eea52fead8feebb4e581b36364f329c896e25
MahRukh <mahrukhdev@gmail.com>
2023-12-15 16:57:08 +0500

    Pet DB and model

---
+++ PetPlanetWebApp/Server/Controllers/DBHelpPet.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.SqlClient;
+
+namespace PetPlanetWebApp.Server.Controllers
+{
+    public class DBHelpPet
+    {
+
+        public static SqlConnection GetConnection()
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-9MFCUJA\\MAH;Initial Catalog=petplanet;Integrated Security=True");
+            return con;
+        }
+    }
+}
